8427be4111514a36a566f8f5b4a00bfd23ceb6cd
Yingying Chen <dinnerparty718@gmail.com>
2023-07-21 18:36:29 -0600

    feat: build images with docker-compose
    Signed-off-by: Yingying Chen <dinnerparty718@gmail.com>

--- .github/actions/build-base-images/docker-compose-build-images.sh
+++ .github/actions/build-base-images/docker-compose-build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -xeEuo pipefail
-trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
+#trap 'RC=$?; echo [error] exit code $RC running $BASH_COMMAND; exit $RC' ERR
 
 # script directory
 HERE="$(cd "$(dirname $0)" && pwd)"
@@ -10,5 +10,5 @@ echo $API_KEY
 echo "Building base images..."
 
 
-# "${HERE}/docker-compose.sh" -f "${HERE}/docker-compose-build-images.yml" build
+"${HERE}/docker-compose.sh" -f "${HERE}/docker-compose-build-images.yml" build
 # "${HERE}/docker-compose.sh" -f "${HERE}/docker-compose-build-images.yml" push
